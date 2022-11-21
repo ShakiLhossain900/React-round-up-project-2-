ad6be1117f65f2111676ff15dec0817894d6a8cd
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-11-21 21:54:18 +0600

    adding

--- src/components/ui/Card.js
+++ src/components/ui/Card.js
@@ -1,10 +1,6 @@
-import classes from './Card.module.css';
-const Card = () => {
-    return (
-        <div className={classes.card}>
-            
-        </div>
-    );
+import classes from "./Card.module.css";
+const Card = (props) => {
+  return <div className={classes.card}>{props.children}</div>;
 };
 
 export default Card;
