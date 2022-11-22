ecbcabe8d69c4c0c994ebd64f134cba6d71fb03d
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-11-22 20:47:27 +0600

    remove the coment

---
+++ src/components/meetups/NewMeetupForm.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Card from '../ui/Card';
+import classes from './NewMeetupForm.module.css';
+
+
+const NewMeetupForm = () => {
+    return (
+        <Card>
+            <form action="">
+               <div></div>
+               <div></div>
+               <div></div>
+               <div></div>
+               <div></div>
+            </form>
+        </Card>
+    );
+};
+
+export default NewMeetupForm;
