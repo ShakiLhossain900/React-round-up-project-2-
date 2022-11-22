0289b9fbb0610ad093608adfa385653bdda32982
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-11-22 20:57:10 +0600

    remove the coment

--- src/pages/AllMeetup.js
+++ src/pages/AllMeetup.js
@@ -15,10 +15,19 @@ const DUMMY_DATA = [
     id: "m2",
     title: "This is a second meetup",
     image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+      "https://cdn.britannica.com/43/93843-050-A1F1B668/White-House-Washington-DC.jpg",
     address: "Meetupstreet 5, 12345 Meetup City",
     description:
       "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+  },
+  {
+    id: "m3",
+    title: "This is a third meetup",
+    image:
+      "https://www.traveloffpath.com/wp-content/uploads/2022/03/off-the-path-florida.jpg",
+    address: "Meetupstreet 5, 12345 Meetup City, USA",
+    description:
+      "This is a third, amazing meetup which you definitely should not miss. It will be a lot of fun!",
   },
 ];
 
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import NewMeetupForm from '../components/meetups/NewMeetupForm';
+import React from "react";
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 const NewMeetupPage = () => {
-    return (
-        <section>
-        <h1>Add New Meetup</h1>
-        <NewMeetupForm/>
-        </section>
-    );
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm />
+    </section>
+  );
 };
 
 export default NewMeetupPage;
