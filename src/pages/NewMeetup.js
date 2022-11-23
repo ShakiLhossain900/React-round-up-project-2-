564dad5992a32507689966e9283cb210f1ca532a
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-11-23 06:49:00 +0600

    adding

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -2,10 +2,15 @@ import React from "react";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 const NewMeetupPage = () => {
+  
+  const addMeetupHandler=() => {
+
+  };
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler}  />
     </section>
   );
 };
