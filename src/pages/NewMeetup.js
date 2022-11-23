e362d9be0c21b048707ab2531ad104b8acbfb0e7
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-11-23 06:57:46 +0600

    adding the fetch

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -2,15 +2,25 @@ import React from "react";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 const NewMeetupPage = () => {
-  
-  const addMeetupHandler=() => {
+  const addMeetupHandler = (meetupData) => {
+    //sending http request to the firebase
 
+    fetch(
+      "https://react-roundup-project2-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        header:{
+          'Content-Type': 'application/json'
+        }
+      }
+    );
   };
 
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm onAddMeetup={addMeetupHandler}  />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 };
