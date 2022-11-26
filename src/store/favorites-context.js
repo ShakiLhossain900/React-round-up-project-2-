578e63b0d6ed86b2d2787f0d8a5f915deadf074b
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-11-26 14:22:02 +0600

    working with user Context

--- src/store/favorites-context.js
+++ src/store/favorites-context.js
@@ -9,7 +9,16 @@ function FavoritesContextProvider(props) {
 
     const [userFavorites, setUserFavorites] = useState([]);
 
-    function addFavorateHandler (){}
+
+    function addFavorateHandler (favoriteMeetup){
+        // setUserFavorites(userFavorites.concat(favoriteMeetup))
+        //uper one use na kore amara  jodi function use kori letest one amra pabu
+       setUserFavorites((prevUserFavorites)=>{
+        return prevUserFavorites.concat(favoriteMeetup);
+       })
+    }
+
+
 
     function removeFavorateHandler (){}
     
