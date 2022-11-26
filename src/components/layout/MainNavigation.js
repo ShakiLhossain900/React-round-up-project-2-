12c5962d7319bb1a83732e681282fe3e88c5c2be
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-11-26 15:43:21 +0600

    working with user Context

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,8 +1,12 @@
-import React from "react";
+import {useContext} from "react";
 import { Link } from "react-router-dom";
+
 import classes from './MainNavigation.module.css';
+import FavoritesContext from "../../store/favorites-context";
 
 const MainNavigation = () => {
+ const favoritesCtx = useContext(FavoritesContext);
+
   return (
     <header className={classes.header}>
       <div className={classes.logo}>React Meetup</div>
--- src/store/favorites-context.js
+++ src/store/favorites-context.js
@@ -1,53 +1,43 @@
-import { createContext,useState } from "react";
+import { createContext, useState } from 'react';
 
 const FavoritesContext = createContext({
   favorites: [],
   totalFavorites: 0,
-
-  addFavorite:(favoriteMeetup) => {},
-  removeFavorite:(meetupId) => {},
-  itemIsFavorite:(meetupId) => {},
-
+  addFavorite: (favoriteMeetup) => {},
+  removeFavorite: (meetupId) => {},
+  itemIsFavorite: (meetupId) => {}
 });
 
 export function FavoritesContextProvider(props) {
+  const [userFavorites, setUserFavorites] = useState([]);
 
-    const [userFavorites, setUserFavorites] = useState([]);
-
-
-    function addFavorateHandler (favoriteMeetup){
-        // setUserFavorites(userFavorites.concat(favoriteMeetup))
-        //uper one use na kore amara  jodi function use kori letest one amra pabu
-       setUserFavorites((prevUserFavorites)=>{
-        return prevUserFavorites.concat(favoriteMeetup);  //The concat() method is used to merge two or more arrays. This method does not change the existing arrays, but instead returns a new array.
-       })
-    }
-
-
-
-    function removeFavorateHandler (meetupId){
-     setUserFavorites(prevUserFavorites=>{
-        return prevUserFavorites.filter(meetup=>meetup.id !== meetupId);
+  function addFavoriteHandler(favoriteMeetup) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.concat(favoriteMeetup);
     });
-    }
-    
-    function itemIsFavoriteHandler (meetupId) {
-        return userFavorites.some(meetup=>meetup.id === meetupId);
-    }
+  }
 
+  function removeFavoriteHandler(meetupId) {
+    setUserFavorites(prevUserFavorites => {
+      return prevUserFavorites.filter(meetup => meetup.id !== meetupId);
+    });
+  }
 
-    const context= {
-        favorites:userFavorites,
-         totalFavorites:userFavorites.length,
-         addFavorite: addFavorateHandler,
-         removeFavorite:removeFavorateHandler,
-         itemIsFavorite:itemIsFavoriteHandler
-    }
+  function itemIsFavoriteHandler(meetupId) {
+    return userFavorites.some(meetup => meetup.id === meetupId);
+  }
 
+  const context = {
+    favorites: userFavorites,
+    totalFavorites: userFavorites.length,
+    addFavorite: addFavoriteHandler,
+    removeFavorite: removeFavoriteHandler,
+    itemIsFavorite: itemIsFavoriteHandler
+  };
 
-    return (
-    <FavoritesContext.Provider value={context} >
-        {props.children}
+  return (
+    <FavoritesContext.Provider value={context}>
+      {props.children}
     </FavoritesContext.Provider>
   );
 }
