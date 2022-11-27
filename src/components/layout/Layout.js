a70e8059fb7baefb3b51d1422affbfb41c407c8e
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-11-27 09:07:48 +0600

    found the error

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import MainNavigation from './MainNavigation';
-import classes from './Layout.module.css';
+import React from "react";
+import MainNavigation from "./MainNavigation";
+import classes from "./Layout.module.css";
 
 const Layout = (props) => {
-    return (
-        <div>
-           <MainNavigation/> 
-           <main className={classes.main}>
-            {props.children}
-           </main>
-        </div>
-    );
+  return (
+    <div>
+      <MainNavigation />
+      <main className={classes.main}>{props.children}</main>
+    </div>
+  );
 };
 
 export default Layout;
